6dabb3043a9f831a7a5b326d78db83dde6a65924
iamherica <hericacadoni@gmail.com>
2021-10-01 22:41:04 -0300

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using usingScaffolding.Models;
+
+namespace usingScaffolding.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace usingScaffolding.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string especie { get; set; }
+        public int quantidade { get; set; }
+        public double valor { get; set; }
+    }
+}
